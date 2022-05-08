22aa92122e1752568dfbabba11fb598793488e4f
J.M. Robin <johnmichaelrobin@gmail.com>
2022-05-08 22:02:10 +0800

    Complete stage 3 implementation

--- coffee-machine/coffee.js
+++ coffee-machine/coffee.js
@@ -12,20 +12,34 @@ Pouring some milk into the cup
 Coffee is ready!
 `);
 
+
 let ingredients = {
     water: 200,
     milk: 50,
     beans: 15
-}
+};
+
+
+console.log("Write how many ml of water the coffee machine has:");
+let storedWater = Number(input()) / ingredients.water;
+let minimum = storedWater;
+
+console.log("Write how many ml of milk the coffee machine has:");
+let storedMilk = Number(input()) / ingredients.milk;
+minimum = storedMilk < minimum ? storedMilk : minimum;
+
+console.log("Write how many grams of coffee beans the coffee machine has:");
+let storedBeans = Number(input()) / ingredients.beans;
+minimum = storedBeans < minimum ? storedBeans : minimum;
+
 
 console.log("Write how many cups of coffee you will need:");
-const cupsOfCoffee = Number(input());
+let cupsOfCoffee = Number(input());
 
-console.log(`For ${cupsOfCoffee} cups of coffee you will need:`)
-for (let ingredient in ingredients) {
-    if (ingredient === 'beans') {
-        console.log(`${ingredients[ingredient] * cupsOfCoffee} g of coffee beans`);
-    } else {
-        console.log(`${ingredients[ingredient] * cupsOfCoffee} ml of ${ingredient}`);
-    }
+if (cupsOfCoffee === parseInt(minimum)) {
+    console.log("Yes, I can make that amount of coffee");
+} else if (cupsOfCoffee < parseInt(minimum)) {
+    console.log(`Yes, I can make that amount of coffee (and even ${parseInt(minimum) - cupsOfCoffee} more than that)`)
+} else {
+    console.log(`No, I can only make ${parseInt(minimum)} cups of coffee`);
 }
