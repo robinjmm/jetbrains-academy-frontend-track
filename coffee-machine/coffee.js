1e422c938090c855b641a810c99dbba70bd78f21
J.M. Robin <johnmichaelrobin@gmail.com>
2022-05-08 20:40:02 +0800

    Complete stage 2 implementation

--- coffee-machine/coffee.js
+++ coffee-machine/coffee.js
@@ -11,3 +11,21 @@ Pouring coffee into the cup
 Pouring some milk into the cup
 Coffee is ready!
 `);
+
+let ingredients = {
+    water: 200,
+    milk: 50,
+    beans: 15
+}
+
+console.log("Write how many cups of coffee you will need:");
+const cupsOfCoffee = Number(input());
+
+console.log(`For ${cupsOfCoffee} cups of coffee you will need:`)
+for (let ingredient in ingredients) {
+    if (ingredient === 'beans') {
+        console.log(`${ingredients[ingredient] * cupsOfCoffee} g of coffee beans`);
+    } else {
+        console.log(`${ingredients[ingredient] * cupsOfCoffee} ml of ${ingredient}`);
+    }
+}
